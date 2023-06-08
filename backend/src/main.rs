72e878c8ea17a524a590f683c537767892496a35
Tomasz Rewak <tomaszrewak@gmail.com>
2023-06-08 19:44:48 +0200

    Fighting a bit with rust.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,7 @@
 use axum::{routing::post, Router};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+use std::sync::Arc;
 use uuid::Uuid;
 
 struct User {
@@ -31,14 +32,48 @@ struct Vote {
     vote_type: VoteType,
 }
 
-async fn vote_handler(vote: axum::extract::Json<Vote>) -> axum::response::Json<Uuid> {
+struct Block {
+    prev: Option<Box<Block>>,
+    vote_type: VoteType,
+}
+
+struct BlockChain {
+    head: Option<Block>,
+}
+
+impl BlockChain {
+    fn new() -> Self {
+        Self { head: None }
+    }
+
+    fn add_block(&mut self, vote_type: VoteType) {
+        let block = Block {
+            prev: self.head.take().map(Box::new),
+            vote_type,
+        };
+        self.head = Some(block);
+    }
+}
+
+struct AppState {
+    block_chain: BlockChain,
+}
+
+async fn vote_handler(vote: axum::extract::Json<Vote>, app_state: axum::extract::State<Arc<AppState>>) -> axum::response::Json<Uuid> {
     let uuid = Uuid::new_v4();
+    let block_chain = & app_state.block_chain;
+
     axum::response::Json(uuid)
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/vote", post(vote_handler));
+    let app_state = Arc::new(AppState {
+        block_chain: BlockChain::new(),
+    });
+    let app = Router::new()
+        .route("/vote", post(vote_handler))
+        .with_state(app_state);
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     axum::Server::bind(&addr)
