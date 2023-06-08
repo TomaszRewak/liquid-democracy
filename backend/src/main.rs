a8d6c2932c12a95ca2d47eafb7f349e20ece06b9
Tomasz Rewak <tomaszrewak@gmail.com>
2023-06-08 18:26:04 +0200

    Basic axum server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,6 @@
+use axum::{routing::post, Router};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
 use uuid::Uuid;
 
 struct User {
@@ -15,6 +18,31 @@ struct Party {
     name: String,
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Deserialize, Serialize)]
+enum VoteType {
+    Yea,
+    Nay,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Vote {
+    user_id: Uuid,
+    poll_id: Uuid,
+    vote_type: VoteType,
+}
+
+async fn vote_handler(vote: axum::extract::Json<Vote>) -> axum::response::Json<Uuid> {
+    let uuid = Uuid::new_v4();
+    axum::response::Json(uuid)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/vote", post(vote_handler));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
