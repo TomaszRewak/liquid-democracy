c5aa5864d8a59c36f49187b0e6b3c8c8f5be9f5f
Tomasz Rewak <tomaszrewak@gmail.com>
2023-06-26 15:18:06 +0200

    Database creation

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,37 @@
 -- psql -U postgres -h 127.0.0.1 -d liquid_democracy -f .../liquid-democracy/database/init.sql
+CREATE TYPE vote_type AS ENUM ('yea', 'nay', 'abstain');
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   password_salt VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+);
+
+CREATE TABLE polls (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE votes (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users(id),
+  poll_id INTEGER NOT NULL REFERENCES polls(id),
+  vote vote_type NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+  CONSTRAINT fk_poll_id FOREIGN KEY (poll_id) REFERENCES polls(id)
+);
+
+CREATE TABLE party (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
--- database/populate.sql
+++ database/populate.sql
@@ -1,6 +1,6 @@
 -- psql -U postgres -h 127.0.0.1 -d liquid_democracy -f .../liquid-democracy/database/populate.sql
-
-INSERT INTO users (name, password, password_salt)
+INSERT INTO
+  users (name, password, password_salt)
 VALUES
   ('Alice', 'password1', 'salt1'),
   ('Bob', 'password2', 'salt2'),
